efd64edd089830ef96ec5b000fe23aaf6e8ec0a3
vijay kumar <vijay@vijay.com>
2023-08-30 16:32:05 -0500

    Project Created

---
+++ ChatbotGSON/src/main/java/com/example/ChatbotGSON/ChatbotGsonApplication.java
@@ -0,0 +1,14 @@
+package com.example.ChatbotGSON;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChatbotGsonApplication {
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(ChatbotGsonApplication.class, args);
+	}
+
+}
---
+++ ChatbotGSON/src/test/java/com/example/ChatbotGSON/ChatbotGsonApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ChatbotGSON;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChatbotGsonApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
